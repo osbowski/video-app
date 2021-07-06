1e99aa7648bc2ec89a0d51d7ea3177dedc1f0621
osbowski <osobiesam@gmail.com>
2021-07-06 08:24:15 +0200

    add-fetch-yt-api-logic

--- src/components/AddVideo.tsx
+++ src/components/AddVideo.tsx
@@ -1,16 +1,9 @@
-import { useState } from "react";
-
 const AddVideo:React.FC = ()=>{
-    const [link,setLink] = useState('');
-
-
     return(
-
-
         <div>
             <h3>Dodaj nowy film do listy</h3>
         <form>
-            <input type="text" onChange={(e)=>setLink(e.target.value)} />
+            <input type="text"/>
             <button>Dodaj</button>
         </form>
         </div>
