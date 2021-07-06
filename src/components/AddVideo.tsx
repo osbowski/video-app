6b2153646a3b958d79a6e256055a83f78da32e40
osbowski <osobiesam@gmail.com>
2021-07-06 08:40:00 +0200

    handle-input-data

--- src/components/AddVideo.tsx
+++ src/components/AddVideo.tsx
@@ -1,10 +1,19 @@
+import React, { useState } from "react";
+
 const AddVideo:React.FC = ()=>{
+    const [link,setLink] = useState('');
+
+    const onSubmit =(e:React.FormEvent)=>{
+        e.preventDefault();
+        console.log(link);
+    }
+
     return(
         <div>
             <h3>Dodaj nowy film do listy</h3>
-        <form>
-            <input type="text" />
-            <button>Dodaj</button>
+        <form onSubmit={onSubmit}>
+            <input type="text" onChange={(e)=>setLink(e.target.value)} />
+            <button type='submit'>Dodaj</button>
         </form>
         </div>
     )
