70f18339fcc13e411176ed119fb941af5806eb1e
osbowski <osobiesam@gmail.com>
2021-07-21 16:34:46 +0200

    pagination-v1

--- src/components/VideoList.tsx
+++ src/components/VideoList.tsx
@@ -1,4 +1,5 @@
 import { useContext, useState } from "react";
+import ReactPaginate from 'react-paginate';
 
 
 import { GlobalContext } from "../context/GlobalState";
@@ -8,15 +9,37 @@ import { fetchedVideo } from "../types";
 
 const VideoList:React.FC = ()=>{
     const {videos} = useContext(GlobalContext);
+    const [pageNumber,setPageNumber] = useState(0);
+    const videosPerPage = 5;
+    const visitedPages = pageNumber * videosPerPage;
 
+    const pageCount = Math.ceil(videos.length/videosPerPage);
+
+    const changePage = ({selected}:any)=>{
+        setPageNumber(selected)
+    }
 
 
     return(
         <div className='d-flex flex-column align-items-center'>
             <h1>Your videos</h1>
-            {videos.map((video:fetchedVideo)=>(
+            {videos.slice(visitedPages, visitedPages + videosPerPage).map((video:fetchedVideo)=>(
                 <VideoListElement key={video.id} video={video} />
             ))}
+        <ReactPaginate
+        previousLabel={"<<"}
+        nextLabel={">>"}
+        pageCount={pageCount}
+        onPageChange={changePage}
+        containerClassName={"paginationBttns"}
+        previousLinkClassName={"previousBttn"}
+        nextLinkClassName={"nextBttn"}
+        disabledClassName={"paginationDisabled"}
+        activeClassName={"paginationActive"}
+        pageRangeDisplayed={5}
+        marginPagesDisplayed={1}
+
+      />
         </div>
     )
 }
