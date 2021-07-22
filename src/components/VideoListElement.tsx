fe61dd0d52ad03fb62d3af3de768653ec9b7abb6
osbowski <osobiesam@gmail.com>
2021-07-22 10:42:48 +0200

    modal-working-on-button

--- src/components/VideoListElement.tsx
+++ src/components/VideoListElement.tsx
@@ -1,8 +1,9 @@
+import { useState } from 'react';
 import { fetchedVideo } from "../types";
 import VideoActions from "./VideoActions"
 import VideoData from "./VideoData"
 import ReactPlayer from 'react-player';
-import { Card, CardBody, CardSubtitle, CardTitle } from "reactstrap";
+import { Button, Card, CardBody, CardSubtitle, CardTitle, Modal, ModalBody } from "reactstrap";
 
 interface VideoListElementProps{
     video:fetchedVideo;
@@ -10,10 +11,20 @@ interface VideoListElementProps{
 
 const VideoListElement:React.FC<VideoListElementProps> = ({video})=>{
     const {link, title} = video.data;
+
+    const [modal, setModal] = useState(false);
+
+    const toggleModal = ()=>setModal(!modal)
+    
     return(
         <Card className={`w-75 my-5 rounded-0 ${video.favorite ? 'bg-warning' : 'bg-light' }`}>
             <CardTitle tag='h5' className='text-center py-3 mb-0'>{title}</CardTitle>
-            <ReactPlayer url={link} light={true} playing={true} controls={true} width={'100%'}/>
+            <Button onClick={toggleModal}>Open Modal</Button>
+            <Modal isOpen={modal} toggle={toggleModal}>
+                <ModalBody>
+                    <ReactPlayer url={link} playing={true} controls={true} width={'100%'}/>
+                </ModalBody>
+            </Modal>
             <CardBody tag='h6' className='px-4 mt-4'>
                 <VideoData data={video.data} />
             </CardBody>
